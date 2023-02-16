1cc3f1b4db5c33ea897f8fd414a98068c182e2dc
Lyne <lyneogola@gmail.com>
2023-02-16 08:11:58 -0800

    task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - programming for creating a exact file plus execute a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
