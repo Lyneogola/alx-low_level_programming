44987001d8a4d3e390f5713b205058c2dbfcce7d
Lyne <lyneogola@gmail.com>
2023-02-17 11:59:25 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: 0(Success)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+		}
+	putchar('\n');
+	return (0);
+}
