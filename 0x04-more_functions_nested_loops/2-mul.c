813ea5858472bc8ed03bc7df2be343f2558f4454
Lyne <lyneogola@gmail.com>
2023-02-23 07:50:59 -0800

    their product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two inters
+ * @x: The first number
+ * @y: The second number
+ * Return: The product of x and y
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
