4cbd9440dc4156906bef77d1eb3d1e2886939042
Lyne <lyneogola@gmail.com>
2023-02-27 14:47:12 -0800

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copirs the string pnt
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int leng = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++
+	}
+
+	*(dest + len) = '\0';
+	return (dest);
+}
