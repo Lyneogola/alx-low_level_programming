62ce73d3118a22dd4ce4d758199cbf6a457006b8
Lyne <lyneogola@gmail.com>
2023-02-27 13:13:34 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main -  Entry pnt
+ * Return: 0(success)
+ */
+
+int main(void)
+
+{
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
