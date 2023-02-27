ecff87c4543c14e25531dfecc23a579c249c453d
Lyne <lyneogola@gmail.com>
2023-02-27 13:35:19 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#inlude "main.h"
+
+/**
+ * _strlen - return the length
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
