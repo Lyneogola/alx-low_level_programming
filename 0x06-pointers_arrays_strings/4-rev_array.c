31e0f9c496d66a1468ff6f9838970246bd587ab9
Lyne <lyneogola@gmail.com>
2023-03-02 08:56:51 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+ * reverse_array - rev
+ * @a: rray
+ * @n: num of element
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
