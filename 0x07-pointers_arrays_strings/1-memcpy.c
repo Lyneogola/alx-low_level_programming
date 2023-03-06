786889096a3616ccbaf86251c1db83fc593c393a
Lyne <lyneogola@gmail.com>
2023-03-06 12:28:09 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies
+ * @dest: memory where ids stored
+ * @src: where copied
+ * @n: num
+ * Return: copird
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+
