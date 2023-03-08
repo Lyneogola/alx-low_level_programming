743cc985d286f063a3650ed161d0a4a5d8215fde
Lyne <lyneogola@gmail.com>
2023-03-08 12:20:52 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,18 +5,34 @@
  * @n: interger
  * Return: evaluate
  */
-int _evaluate(int i, int n)
+int _evaluate(int n, int i)
 {
 	if (n == 0 || n == 1)
 		return (n);
 
 	else if (i * i < n)
-		return (_evaluate(i + 1, n));
+		return (_evaluate(1 + 1, n));
 
 	else if (i * i == n)
 		return (i);
-
 	return (-1);
 
 	return (-1);
+}
+/**
+ * _sqrt_recursion - evaluate squrt
+ * @n: interger
+ * Return: fun
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n));
+	}
 }
