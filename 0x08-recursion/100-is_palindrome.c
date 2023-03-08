4f4c638baf1304668b19b2662047791c34abf5f8
Lyne <lyneogola@gmail.com>
2023-03-08 13:10:55 -0800

    be tough

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+nclude "main.h"
+
+/**
+ * _strlen_recursion - size
+ * @s: pointer
+ * Return: pnt
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + 1))
+	{
+		return (p1(s + 1, 1 - 2));
+	}
+	return (0);
+}
