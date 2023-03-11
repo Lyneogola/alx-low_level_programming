e120031a7a3e54ed4f387aeb145575efe0b2602f
Lyne <lyneogola@gmail.com>
2023-03-11 00:45:00 -0800

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry pont
+ * @argc: count
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
